49986360896271466165c96f7fce330d9b15eaf8
puroome <129413484+puroome@users.noreply.github.com>
2026-01-21 01:23:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2532,8 +2532,8 @@ const learningMode = {
         this.elements.explanationContainer.classList.toggle('hidden', !wordData.explanation?.trim());
 
         const hasSample = wordData.sample && wordData.sample.trim() !== '';
-        const sampleImgUrl = 'https://images.icon-icons.com/1055/PNG/128/14-delivery-cat_icon-icons.com_76690.png';
-        const noSampleImgUrl = 'https://images.icon-icons.com/1055/PNG/128/19-add-cat_icon-icons.com_76695.png';
+        const sampleImgUrl = 'images/cat-delivery.png';
+        const noSampleImgUrl = 'images/cat-add.png';
         this.elements.sampleBtnImg.src = await imageDBCache.loadImage(hasSample ? sampleImgUrl : noSampleImgUrl);
 
         this.updateFavoriteIcon(utils.isFavorite(wordData.word));
