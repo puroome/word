24aea425b8cd4258229da6b960502a17e94bb876
puroome <129413484+puroome@users.noreply.github.com>
2026-01-28 23:14:40 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+export const config = {
+    TTS_API_KEY: "AIzaSyAJmQBGY4H9DVMlhMtvAAVMi_4N7__DfKA",
+    DEFINITION_API_KEY: "02d1892d-8fb1-4e2d-bc43-4ddd4a47eab3",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzyBM33LzFsAe-mES_0Qw5B8w0ZPyYTDm4K_nLif5y2bXMpiQbD1LX5TTIDA4qX_Rnp/exec",
+    ALLOWED_USER_EMAIL: "puroome@gmail.com",
+    FIREBASE: {
+        apiKey: "AIzaSyAX-cFBU45qFZTAtLYPTolSzqqLTfEvjP0",
+        authDomain: "word-91148.firebaseapp.com",
+        databaseURL: "https://word-91148-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "word-91148",
+        storageBucket: "word-91148.firebasestorage.app",
+        messagingSenderId: "53576845185",
+        appId: "1:53576845185:web:f519aa3ec751e12cb88a80"
+    }
+};
+
+export const LOCAL_STORAGE_KEYS = {
+    TTS_VOICE: 'student_ttsVoice',
+    LAST_INDEX: 'student_lastIndex_main',
+    UNSYNCED_TIME: 'student_unsyncedTime_main',
+    UNSYNCED_QUIZ: 'student_unsyncedQuizStats_main',
+    UNSYNCED_PROGRESS_UPDATES: 'student_unsyncedProgress_main',
+    PRACTICE_MODE: 'student_practiceMode_main',
+    QUIZ_RANGE_START: 'student_quizRangeStart_main',
+    QUIZ_RANGE_END: 'student_quizRangeEnd_main'
+};
