2af3888de79510f4cc333cc2f7ea75dc8fa51184
puroome <129413484+puroome@users.noreply.github.com>
2026-02-03 09:06:58 +0900

    Update learning.js

--- js/learning.js
+++ js/learning.js
@@ -111,9 +111,7 @@ export const learningMode = {
             if(e.target.closest('button')) return; 
             preventCardMenu(e, 'back');
         });
-
-        this.elements.learningModeContainer.addEventListener('contextmenu', (e) => this.handleCardContextMenu(e));
-
+        
         this.elements.editContextBtn.addEventListener('click', () => {
             this.enterEditMode(this.state.editingSide);
             ui.hideEditContextMenu();
